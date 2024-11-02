a20b503930fb483f76b0a9df9cb27c327dba2e0f
Ashish Alexander <ashishalex29@gmail.com>
2024-11-02 16:02:57 +0530

    admin user and recuriter mgm including block

---
+++ src/app/admin/adminPannel/recruiterManagement/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { RecruiterTable } from './components/RecruiterTable'; 
+
+const RecuriterManagementPage = () => {
+  return (
+    <div>
+      <h1>Recuriter Management</h1>
+      <RecruiterTable />
+    </div>
+  );
+};
+
+export default RecuriterManagementPage; 
